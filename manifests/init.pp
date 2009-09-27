01727041a5443bd66d2b12173456e0898adfacbb
Richard Crowley <r@rcrowley.org>
2009-09-27 17:35:26 +0000

    Better Ruby, soon to be ported into the other languages.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,17 +1,18 @@
 define geminstall($version) {
 	exec { "install-rubygems-1.3.5-$version":
-		require => Exec["extract-rubygems-1.3.5"],
-		before => Exec["remove-rubygems-1.3.5"],
+		require => [
+			Exec["extract-rubygems-1.3.5"],
+			Sourceinstall["ruby-$version"]
+		],
 		cwd => "/root/rubygems-1.3.5",
-		command => "/opt/ruby-$version/bin/ruby setup.rb --prefix=/opt/ruby-$version",
-		creates => "/opt/ruby-$version/bin/gem",
-		onlyif => "test -d /opt/ruby-$version",
+		command => "/opt/ruby-$version/bin/ruby setup.rb",
+		unless => "test 1.3.5 = $(/opt/ruby-$version/bin/gem --version)",
 	}
 }
 
 define gem($name, $version) {
 	exec { "install-gem-$name-$version":
-		require => Geminstall["gem-1.3.5-$version"],
+		require => Geminstall["rubygems-1.3.5-$version"],
 		command => "/opt/ruby-$version/bin/gem install $name",
 	}
 }
@@ -20,31 +21,31 @@ class ruby {
 	include ruby::ruby_1_9_1
 	include ruby::ruby_1_8_7
 
-	file { "/opt/rubygems-1.3.5.tgz":
-		require => [
-			Sourceinstall["ruby-1.9.1-p243"],
-			Sourceinstall["ruby-1.8.7-p174"]
-		],
-		before => Exec["extract-rubygems-1.3.5"],
+	file { "/root/rubygems-1.3.5.tgz":
 		source => "puppet://$servername/ruby/rubygems-1.3.5.tgz",
 		ensure => present,
 	}
 	exec { "extract-rubygems-1.3.5":
-		require => File["/opt/rubygems-1.3.5.tgz"],
-		before => [
-			Geminstall["rubygems-1.3.5-1.9.1-p243"],
-			Geminstall["rubygems-1.3.5-1.8.7-p174"]
-		],
+		require => File["/root/rubygems-1.3.5.tgz"],
 		cwd => "/root",
-		command => "tar xf /opt/rubygems-1.3.5.tgz",
+		command => "tar xf /root/rubygems-1.3.5.tgz",
 		creates => "/root/rubygems-1.3.5",
 		#unless => "UNLESS WHAT???",
 	}
-	# If anything happened in order, each pipinstall would go here
+	# If anything happened in order, each geminstall would go here
 	exec { "remove-rubygems-1.3.5":
-		cwd => "/root",
-		command => "rm -rf rubygems-1.3.5",
-		onlyif => "test -d /root/rubygems-1.3.5",
+		require => [
+			Geminstall["rubygems-1.3.5-1.9.1-p243"],
+			Geminstall["rubygems-1.3.5-1.8.7-p174"]
+		],
+		command => "rm -rf /root/rubygems-1.3.5",
+	}
+	exec { "remove-tar-rubygems-1.3.5":
+		require => [
+			Geminstall["rubygems-1.3.5-1.9.1-p243"],
+			Geminstall["rubygems-1.3.5-1.8.7-p174"]
+		],
+		command => "rm -f /root/rubygems-1.3.5.tgz",
 	}
 
 	file { "/usr/local/bin/pick-ruby":
